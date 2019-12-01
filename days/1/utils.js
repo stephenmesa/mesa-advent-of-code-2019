8ca6959c2656e62cc9766ebdffd0de2540807402
Stephen Mesa <stephen.mesa@gmail.com>
2019-12-01 09:19:03 -0800

    Make test green

--- days/1/utils.js
+++ days/1/utils.js
@@ -1,9 +1,15 @@
 const calculateModuleFuel = (mass) => {
+
     return Math.floor(mass / 3) - 2;
 };
 
 const calculateTotalModuleFuel = (mass) => {
-
+    const fuel = calculateModuleFuel(mass);
+    if (fuel < 0) {
+        return 0;
+    } else {
+        return fuel + calculateTotalModuleFuel(fuel);
+    }
 };
 
 module.exports = {
