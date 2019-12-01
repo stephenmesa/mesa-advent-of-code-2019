71ce822bea43ed693bd1a3d4970da4670bbf84f4
Stephen Mesa <stephen.mesa@gmail.com>
2019-12-01 09:04:51 -0800

    Use the working function

--- days/1/index.js
+++ days/1/index.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
+const { calculateModuleFuel } = require('./utils');
+
+const isValidNumber = d =>
+    d && d.length > 0 && !Number.isNaN(Number(d));
 
 fs.readFile('input.txt', "utf8", (err, data) => {
     if (err) {
         console.error('Error reading input.txt file', err);
         process.exit(1);
     }
-    const inputValues = data.split('\n').filter(d => d && d.length > 0);
-    console.log({ inputValues });
+    const validInputValues = data.split('\n').filter(isValidNumber);
+    const moduleMasses = validInputValues.map(v => Number(v));
+    const moduleFuels = moduleMasses.map(calculateModuleFuel);
+    console.log({ moduleMasses });
+    console.log({ moduleFuels });
 });
