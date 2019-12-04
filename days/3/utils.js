1243206efca5cd38617708d19ff3503e9217c5e6
Stephen Mesa <stephen.mesa@gmail.com>
2019-12-03 20:38:51 -0800

    Dry it up

--- days/3/utils.js
+++ days/3/utils.js
@@ -5,54 +5,7 @@ const parseCommands = (inputs) => {
     });
 }
 
-const getNearestIntersectionDistance = (wiresInput) => {
-    const [wire1Inputs, wire2Inputs] = wiresInput.split('\n');
-
-    const wire1Commands = parseCommands(wire1Inputs);
-    const wire2Commands = parseCommands(wire2Inputs);
-
-    const visitedCoords = {};
-
-    let cursorX = 0;
-    let cursorY = 0;
-
-    moveCursorOperationLookup = {
-        U: () => cursorY++,
-        D: () => cursorY--,
-        L: () => cursorX--,
-        R: () => cursorX++,
-    };
-
-    for(let i = 0; i < wire1Commands.length; i++) {
-        const command = wire1Commands[i];
-        for(let x = 0; x < command.amount; x++) {
-            moveCursorOperationLookup[command.direction]();
-            visitedCoords[`${cursorX},${cursorY}`] = { x: cursorX, y: cursorY };
-        }
-    }
-
-    const intersections = [];
-
-    // Restart coordinated back at 0, 0
-    cursorX = 0;
-    cursorY = 0;
-
-    for(let i = 0; i < wire2Commands.length; i++) {
-        const command = wire2Commands[i];
-        for(let x = 0; x < command.amount; x++) {
-            moveCursorOperationLookup[command.direction]();
-            if (visitedCoords[`${cursorX},${cursorY}`]) {
-                intersections.push({ x: cursorX, y: cursorY });
-            }
-        }
-    }
-
-    const distances = intersections.map(c => Math.abs(c.x) + Math.abs(c.y));
-
-    return Math.min(...distances);
-};
-
-const getFewestStepsIntersectionSteps = (wiresInput) => {
+const getIntersections = (wiresInput) => {
     const [wire1Inputs, wire2Inputs] = wiresInput.split('\n');
 
     const wire1Commands = parseCommands(wire1Inputs);
@@ -96,11 +49,23 @@ const getFewestStepsIntersectionSteps = (wiresInput) => {
             stepCounter++;
             const intersectingCoord = visitedCoords[`${cursorX},${cursorY}`];
             if (intersectingCoord) {
-                intersections.push({ steps: intersectingCoord.steps + stepCounter });
+                intersections.push({ x: cursorX, y: cursorY, steps: intersectingCoord.steps + stepCounter });
             }
         }
     }
 
+    return intersections;
+}
+
+const getNearestIntersectionDistance = (wiresInput) => {
+    const intersections = getIntersections(wiresInput);
+    const distances = intersections.map(c => Math.abs(c.x) + Math.abs(c.y));
+
+    return Math.min(...distances);
+};
+
+const getFewestStepsIntersectionSteps = (wiresInput) => {
+    const intersections = getIntersections(wiresInput);
     const steps = intersections.map(i => i.steps);
     return Math.min(...steps);
 };
